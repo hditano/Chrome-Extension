7aaca243a996bfc8e6d33b1bcfa196849b62a619
Hernan Di Tano <hditano@gmail.com>
2021-08-08 01:14:34 -0300

    first version deployed

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-let myLeads = ["Hola", "Hernancito"];
+let myLeads = [];
 const inputEl = document.querySelector("#input-el");
 const UlEl = document.querySelector("#ul-el");
 
 let saveButton = document.querySelector("#input-btn");
+
 
 saveButton.addEventListener("click", function () {
   myLeads.push(inputEl.value);
@@ -10,14 +11,20 @@ saveButton.addEventListener("click", function () {
   leadItems();
 });
 
+
+// Looping through myLeads array
+
 function leadItems() {
   let listItems = "";
+ 
   for (let i = 0; i < myLeads.length; i++) {
-    listItems += "<li>" + myLeads[i] + "</li>";
+    listItems += `<li><a target='_blank' href=http://${myLeads[i]}>${myLeads[i]}</a></li>`;
+
     // const li = document.createElement('li');
     // li.textContent = myLeads[i];
     // UlEl.append(li);
   }
+
   UlEl.innerHTML = listItems;
 }
 
