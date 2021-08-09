9937d6c6c0c2977fda50932efb29d1713562a86c
Hernan Di Tano <hditano@gmail.com>
2021-08-09 06:59:05 -0300

    added save tab function. added chrome.tab.query handling

--- script.js
+++ script.js
@@ -2,10 +2,12 @@ let myLeads = [];
 const inputEl = document.querySelector("#input-el");
 const UlEl = document.querySelector("#ul-el");
 
-let saveButton = document.querySelector("#input-btn");
+const deleteButton = document.querySelector("#delete-btn");
+const saveButton = document.querySelector("#input-btn");
+const tabButton = document.querySelector("#tab-btn");
 
 // Parses values from local Storage (string) and sets its value to leadsFromLocalStorage as an array
-let leadsFromLocalStorage = JSON.parse(localStorage.getItem('myleads'));
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myleads"));
 
 // Checks for Local Storage values, if so, store it in myLeads Array
 if (leadsFromLocalStorage) {
@@ -14,25 +16,41 @@ if (leadsFromLocalStorage) {
 }
 
 saveButton.addEventListener("click", function () {
-  myLeads.push(inputEl.value);
-  inputEl.value = "";
-  localStorage.setItem('myleads', JSON.stringify(myLeads));
-  leadItems();
+    myLeads.push(inputEl.value);
+    inputEl.value = "";
+    localStorage.setItem("myleads", JSON.stringify(myLeads));
+    leadItems();
 });
 
+
+tabButton.addEventListener("click", function () {
+    chrome.tabs.query({active: true, lastFocusedWindow: true}, function(tabs) {
+        myLeads.push(tabs[0].url);
+        inputEl.value = "";
+        localStorage.setItem('myleads', JSON.stringify(myLeads));
+        leadItems();
+    })
+});
+
+// Deletes local Storage // deletes items in array myLeads // clears DOM calling out to render leadItems()
+deleteButton.addEventListener("dblclick", function () {
+    localStorage.clear();
+    myLeads = [];
+    leadItems();
+});
 
 // Looping through myLeads array
 
 function leadItems() {
-  let listItems = "";
- 
-  for (let i = 0; i < myLeads.length; i++) {
-    listItems += `<li><a target='_blank' href=http://${myLeads[i]}>${myLeads[i]}</a></li>`;
+    let listItems = "";
 
-    // const li = document.createElement('li');
-    // li.textContent = myLeads[i];
-    // UlEl.append(li);
-  }
+    for (let i = 0; i < myLeads.length; i++) {
+        listItems += `<li><a target='_blank' href=http://${myLeads[i]}>${myLeads[i]}</a></li>`;
 
-  UlEl.innerHTML = listItems;
+        // const li = document.createElement('li');
+        // li.textContent = myLeads[i];
+        // UlEl.append(li);
+    }
+
+    UlEl.innerHTML = listItems;
 }
