d4f3f71981d7286b335d858ba56c4230776af81d
Hernan Di Tano <hditano@gmail.com>
2021-08-10 02:10:50 -0300

    finished delete icon function. Added image grow and transition

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const UlEl = document.querySelector("#ul-el");
 const deleteButton = document.querySelector("#delete-btn");
 const saveButton = document.querySelector("#input-btn");
 const tabButton = document.querySelector("#tab-btn");
-const deleteIcon = document.querySelector("#delete-icon");
+const deleteIcon = document.querySelectorAll("#delete-icon");
 
 // Parses values from local Storage (string) and sets its value to leadsFromLocalStorage as an array
 const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myleads"));
@@ -50,7 +50,7 @@ function leadItems() {
     let listItems = "";
 
     for (let i = 0; i < myLeads.length; i++) {
-        listItems += `<li><a target='_blank' href=http://${myLeads[i]}>${myLeads[i]}</a> <img id="delete-icon" alt="" src="delete-icon.png"> </li>`;
+        listItems += `<li data-id="${i}"><a target='_blank' href=http://${myLeads[i]}>${myLeads[i]}</a> <img id="delete-icon" alt="" src="delete-icon.png"> </li>`;
         // const li = document.createElement('li');
         // li.textContent = myLeads[i];
         // UlEl.append(li);
@@ -59,9 +59,16 @@ function leadItems() {
     UlEl.innerHTML = listItems;
 }
 
+
 // Event Listener that checks if certain id exists...if so, if statement comes true and works. (Event Delegation)
+// Data-ID variable gets the data-id associate to his <li> parent.
+// Parses myleads localStorage, deletes myLeads specific value and Re-set it in the Local Storage, calls render.
 document.body.addEventListener('click', function (e) {
-    if (e.target.id == 'delete-icon'){
-        console.log('clicked');
+    if (e.target.id === 'delete-icon') {
+        let dataID = e.target.parentNode.getAttribute('data-id');
+        let items = JSON.parse(localStorage.getItem('myleads'));
+        myLeads.splice(dataID, 1);
+        localStorage.setItem('myleads', JSON.stringify(myLeads));
+        leadItems();
     }
 })
